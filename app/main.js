7fd325577fb041d46e41076da66aa4714885d2f2
Thomas Collardeau <thomas@collardeau.com>
2015-10-03 15:36:38 +0200

    add realtime filter for min temperature

--- app/main.js
+++ app/main.js
@@ -5,44 +5,54 @@ import {cities$} from '../mockServer';
 
 let Ob$ = Cycle.Rx.Observable;
 
+function renderMinTempSlider(minTemp){
+  return (
+    <div>
+      <input id='minTemp' type='range' min='-20' max='40' value= {minTemp} />
+      min temp: { minTemp }
+    </div> 
+  )
+}
+
+function renderCity(city) {
+  if (!city) { return null }
+  return (
+    <div>
+      <h3>{city.name}</h3>
+      <p> min temp over the last 7 days: <b>{ city.minTemp }</b>C</p>
+    </div>
+  )
+};
+
 function intent(DOM){
 
  return {
     requestCities$: cities$,
     changeMinTemp$ : DOM.select('#minTemp').events('input')
       .map(e => e.target.value)
-      .debounce(100),
+      .debounce(10),
     changeMaxCloud$ : DOM.select('#maxCloud').events('input')
       .map(e => e.target.value)
-      .debounce(100),
+      .debounce(10),
   };
 }
 
 function model(actions){ 
-
   return Cycle.Rx.Observable.combineLatest(
     actions.changeMinTemp$.startWith(10),
     actions.changeMaxCloud$.startWith(20),
     actions.requestCities$.startWith([]),
-    (minTemp, maxCloud, cities) => ({minTemp, maxCloud, cities})
+    (minTemp, maxCloud, cities) => ({
+      cities: cities.filter(city => city ? city.minTemp > minTemp: null),
+      minTemp, 
+      maxCloud
+    })
   );
 }
 
-function renderMinTempSlider(minTemp){
-  return (
-    <div>
-      <input id='minTemp' type='range' min='-20' max='40' value= {minTemp} />
-      { minTemp }
-    </div> 
-  )
-}
-
-function renderCity(city) {
-  return <div>{city ? city.name : 'city'}</div>
-};
-
 function view(state$) {
   return state$.map(({minTemp, maxCloud, cities}) => {
+    console.log(cities);
    return (
       <div>
         { renderMinTempSlider(minTemp) }
--- mockServer.js
+++ mockServer.js
@@ -5,20 +5,41 @@ let barcelona = require('json!./sample-data/barcelona.json');
 let berlin = require('json!./sample-data/berlin.json');
 let london = require('json!./sample-data/london.json');
 
+let calcMinTemp = (arrOfForecasts) => {
+  return arrOfForecasts.reduce((acc, curr) => {
+    return curr.temp.min < acc ? curr.temp.min : acc;  
+  }, 100)
+}
+
 let barcelona$ = Ob$.just(barcelona)
-.delay(800).map(city => ({name: city.city.name, forecasts: city.list}));
+.delay(800)
+.map(city => ({
+  name: city.city.name, 
+  forecasts: city.list,
+  minTemp: calcMinTemp(city.list)
+}));
 
-let berlin$ = Ob$.just(berlin).delay(1200)
-.delay(600).map(city => ({name: city.city.name, forecasts: city.list}));
+let berlin$ = Ob$.just(berlin)
+.delay(1600)
+.map(city => ({
+  name: city.city.name, 
+  forecasts: city.list,
+  minTemp: calcMinTemp(city.list)
+}));
 
-let london$ = Ob$.just(london).delay(400)
-.delay(1200).map(city => ({name: city.city.name, forecasts: city.list}));
+let london$ = Ob$.just(london)
+.delay(2200)
+.map(city => ({
+  name: city.city.name, 
+  forecasts: city.list,
+  minTemp: calcMinTemp(city.list)
+}));
 
 export let cities$ =  Ob$.combineLatest(
-    barcelona$.startWith(null),
-    berlin$.startWith(null),
-    london$.startWith(null),
-    (bar, ber, lon) => [bar, ber, lon]
+  barcelona$.startWith(null),
+  berlin$.startWith(null),
+  london$.startWith(null),
+  (bar, ber, lon) => [bar, ber, lon]
 );
  
 
