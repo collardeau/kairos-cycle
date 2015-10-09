db967a5aff4d93ab20ef88acdbff595947a4ca5b
Thomas Collardeau <thomas@collardeau.com>
2015-10-09 20:46:19 +0200

    send cities down to browser

--- server.js
+++ server.js
@@ -7,32 +7,45 @@ var Ob$ = Cycle.Rx.Observable;
 
 var URL = 'http://api.openweathermap.org/data/2.5/forecast/daily';
 
-var moscow = new RSVP.Promise(function(resolve, rej) {
-  superagent
-  .get(URL)
-  .set('x-api-key','5ba09e308c10daeb4737c29d3fef2907')
-  .query({ 
-    id: 524901, 
-    cnt: 7 
-  })
-  .end(function(err, res) {
-    resolve(res.body);
-  });
-});
+var bcn = fetchCity('barcelona');
+var cop = fetchCity('copenhagen');
+var bcn$ = Ob$.fromPromise(bcn);
+var cop$ = Ob$.fromPromise(cop);
 
+var cities$ = Ob$.combineLatest(
+  bcn$,
+  cop$,
+  ((a, b) => [a, b])
+)
+
+function fetchCity(cityName) {
+  return new RSVP.Promise(function(resolve, rej) {
+    superagent
+    .get(URL)
+    .set('x-api-key','5ba09e308c10daeb4737c29d3fef2907')
+    .query({ 
+      q: cityName,
+      cnt: 7 
+    })
+    .end(function(err, res) {
+      resolve(res.body);
+    });
+  })
+}
+
+var lastUpdated = 1;
 app.get('/cities', function(req, res) {
-  moscow.then(d => {
-    res.json(d); 
-  })
+  if(lastUpdated) {  
+    cities$.subscribe(cities => {
+      res.json(cities);
+    })
+  }
 });
 
-
-//var source$ = Ob$.fromPromise(moscow);
-//source$.subscribe(x => console.log(x));
+app.use(express.static('public'));
 
 app.listen(3000, function(){
   console.log('listening on port 3000...');
 });
-//
 
 
