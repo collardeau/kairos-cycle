293d7fec4a8a9b40bc8a9388953338d3af47c26d
Thomas Collardeau <thomas@collardeau.com>
2015-10-09 11:25:13 +0200

    add morgan logger

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 var express = require('express');
+var logger = require('morgan');
 var app = express();
+
+
+app.use(logger("combined"));
 
 app.use(function(req, res) {
   res.writeHead(200, { "Content-Type": "text/plain"});
