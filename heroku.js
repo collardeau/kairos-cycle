cf79877623d7da08d541d6e8279c120bf20300c7
Thomas Collardeau <thomas@collardeau.com>
2015-10-10 14:37:05 +0200

    fix heroku postprocess

--- heroku.js
+++ heroku.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production') {
   var child_process = require('child_process');
-  var proc = "webpack -p --config webpack.production.config.js";
+  var proc = "webpack -p --config webpack-prod.config.js";
   child_process.exec(proc, function(error, stdout, stderr) {
     console.log('stdout: ' + stdout);
     console.log('stderr: ' + stderr);
