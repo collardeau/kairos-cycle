86f7fba5ad41c9980008edc274a8ec38676abdd0
Thomas Collardeau <thomas@collardeau.com>
2015-10-10 20:11:22 +0200

    adjust city fetch timers

--- server.js
+++ server.js
@@ -29,19 +29,19 @@ function fetchCity(cityName) {
 var oneHour = 1000 * 60 * 60;
 var bar$ = Ob$.timer(0, oneHour).flatMap(x => fetchCity('barcelona'));
 var ber$ = Ob$.timer(10, oneHour).flatMap(x => fetchCity('berlin'));
-var cap$ = Ob$.timer(70, oneHour).flatMap(x => fetchCity('capri'));
-var cop$ = Ob$.timer(70, oneHour).flatMap(x => fetchCity('copenhagen'));
+var cap$ = Ob$.timer(90, oneHour).flatMap(x => fetchCity('capri'));
+var cop$ = Ob$.timer(80, oneHour).flatMap(x => fetchCity('copenhagen'));
 var fra$ = Ob$.timer(70, oneHour).flatMap(x => fetchCity('frankurt'));
-var ist$ = Ob$.timer(20, oneHour).flatMap(x => fetchCity('istanbul'));
+var ist$ = Ob$.timer(100, oneHour).flatMap(x => fetchCity('istanbul'));
 var lis$ = Ob$.timer(20, oneHour).flatMap(x => fetchCity('lisbon'));
-var lon$ = Ob$.timer(20, oneHour).flatMap(x => fetchCity('london'));
+var lon$ = Ob$.timer(120, oneHour).flatMap(x => fetchCity('london'));
 var mad$ = Ob$.timer(80, oneHour).flatMap(x => fetchCity('madrid'));
-var mia$ = Ob$.timer(50, oneHour).flatMap(x => fetchCity('miami'));
+var mia$ = Ob$.timer(150, oneHour).flatMap(x => fetchCity('miami'));
 var myk$ = Ob$.timer(50, oneHour).flatMap(x => fetchCity('mykonos'));
 var nic$ = Ob$.timer(60, oneHour).flatMap(x => fetchCity('nice'));
-var nyc$ = Ob$.timer(30, oneHour).flatMap(x => fetchCity('newyork'));
-var par$ = Ob$.timer(30, oneHour).flatMap(x => fetchCity('paris'));
-var sev$ = Ob$.timer(30, oneHour).flatMap(x => fetchCity('sevilla,es'));
+var nyc$ = Ob$.timer(130, oneHour).flatMap(x => fetchCity('newyork'));
+var par$ = Ob$.timer(110, oneHour).flatMap(x => fetchCity('paris'));
+var sev$ = Ob$.timer(140, oneHour).flatMap(x => fetchCity('sevilla,es'));
 var sfc$ = Ob$.timer(30, oneHour).flatMap(x => fetchCity('sanfrancisco, us'));
 
 var cities$ = Ob$.combineLatest(
